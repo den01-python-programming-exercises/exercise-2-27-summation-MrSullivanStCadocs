91cf8190302ed335562db7ff73ce127a91ef9a60
MrSullivan <>
2021-05-17 21:38:04 +0000

    two def, return and sum and print command as string

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    answer = sum(4,3,6,1)
+    print("Sum: " + str(answer))
+
+def sum(number1, number2, number3, number4):
+  return number1 + number2 + number3 + number4
 
 if __name__ == '__main__':
     main()
